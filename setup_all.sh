9bb9974b2c081ad43f2bfcdff0e7e768ff7074c2
Brendan Chan <57857423+brendanc2122@users.noreply.github.com>
2022-09-12 14:00:14 +0800

    Update setup_all.sh

--- setup_all.sh
+++ setup_all.sh
@@ -4,4 +4,4 @@ TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.ge
 TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
 # Linux (pip)
-g++ -std=c++11 -shared coref_kernels.cc -o coref_kernels.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 #-D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 -shared coref_kernels.cc -o coref_kernels.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 -D_GLIBCXX_USE_CXX11_ABI=0
